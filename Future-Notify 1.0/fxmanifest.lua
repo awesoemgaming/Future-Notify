0a1b1e119b846305b6e16d80ac39d37326552b65
awesoemgaming <129987735+awesoemgaming@users.noreply.github.com>
2023-05-05 20:28:43 -0700

    Create fxmanifest.lua

---
+++ Future-Notify 1.0/fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'AwesomeGaming'
+description 'Future Notification system'
+version '1.0.0'
+
+ui_page 'html/index.html'
+
+client_scripts {
+	'client.lua',
+}
+
+files {
+	'html/*.*'
+}
+
+export 'Alert'
